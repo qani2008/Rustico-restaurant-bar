89682ea44759a19ef48ed0bbe12ba97cb36876e3
qani2008 <qani@demiri.co.uk>
2025-02-12 01:26:11 +0000

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,5 @@
+
+document.getElementById("reservation-form").addEventListener("submit", function(event) {
+    event.preventDefault();
+    alert("Your reservation has been made successfully!");
+});
